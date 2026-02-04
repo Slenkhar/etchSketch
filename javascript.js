9f6f7f916499b4ae0af3b33e21d5a1c0e9e84ed1
David Secretain <slenkhar@gmail.com>
2026-02-04 14:33:07 +0000

    Create hover logic
    also contains random color and opacity increment on hover

--- javascript.js
+++ javascript.js
@@ -1,15 +1,16 @@
 const gridContainer = document.querySelector("#gridContainer");
 const gridReset = document.querySelector("#gridReset");
 
-//----------------
+//-------------
 // Create grid
-//----------------
+//-------------
 gridReset.addEventListener("click", createGrid);
 
 function createGrid() {
   gridContainer.innerHTML = "";
 
-  let size = +prompt("How many squares per side? (16/20/32/40/50): ");
+  let size = +prompt("How many squares per side? (max 50): ");
+  size = size > 50 ? 50 : size;
   const percent = 100 / size;
 
   document.documentElement.style.setProperty("--grid-percent", `${percent}%`);
@@ -18,5 +19,29 @@ function createGrid() {
     let gridElement = document.createElement("div");
     gridElement.className = "gridElement";
     gridContainer.appendChild(gridElement);
+    gridElement.style.backgroundColor = setStartColor();
+    gridElement.style.opacity = 0;
   }
+}
+
+//------------------
+// Hover Etch logic
+//------------------
+
+gridContainer.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("gridElement")) {
+    e.target.classList.add("hovered");
+    e.target.style.opacity = Number(e.target.style.opacity) + 0.1;
+  }
+});
+
+//------------------
+// Helper functions
+//------------------
+
+function setStartColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
 }
