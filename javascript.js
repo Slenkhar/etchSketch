f03a1ab63bfd8461c74ef9ecefac94f52c805382
David Secretain <slenkhar@gmail.com>
2026-02-04 12:55:57 +0000

    Create grid logic
    grid created dynamically by user prompt, sizing handled by CSS variable

--- javascript.js
+++ javascript.js
@@ -1,15 +1,22 @@
 const gridContainer = document.querySelector("#gridContainer");
-
-//----------------
-//Define grid size
-//----------------
-const gridSize = 16 * 16;
+const gridReset = document.querySelector("#gridReset");
 
 //----------------
 // Create grid
 //----------------
-for (let i = 0; i < gridSize; i++) {
-  let gridElement = document.createElement("div");
-  gridElement.className = "gridElement";
-  gridContainer.appendChild(gridElement);
+gridReset.addEventListener("click", createGrid);
+
+function createGrid() {
+  gridContainer.innerHTML = "";
+
+  let size = +prompt("How many squares per side? (16/20/32/40/50): ");
+  const percent = 100 / size;
+
+  document.documentElement.style.setProperty("--grid-percent", `${percent}%`);
+
+  for (let i = 0; i < size * size; i++) {
+    let gridElement = document.createElement("div");
+    gridElement.className = "gridElement";
+    gridContainer.appendChild(gridElement);
+  }
 }
